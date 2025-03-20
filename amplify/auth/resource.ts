6985dd91d1c6bc8b1c64ba8b1941e0b638bcf018
mhgapps <jrodriguez@markethospitalitygroup.com>
2025-03-20 15:13:00 -0400

    0.0.1

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,3 +1,11 @@
-# amplify/
-# amplify_outputs*
-# amplifyconfiguration*
+import { defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+});
