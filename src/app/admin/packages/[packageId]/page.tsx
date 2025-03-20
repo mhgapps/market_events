d9ccaad0a795c87f540409870894418c31d8246f
mhgapps <jrodriguez@markethospitalitygroup.com>
2025-03-20 14:42:09 -0400

    Add Amplify auth configuration

---
+++ src/app/admin/packages/[packageId]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function EventContractPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Contract Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
